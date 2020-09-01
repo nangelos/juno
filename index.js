e90efae6f67531e93f57e6f302232f171ff1a4dc
nangelos <nicksangelos@gmail.com>
2020-09-01 16:45:39 -0400

    [html & js] better way to add event listener

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-console.log('index.js linked up');
-let posts = document.getElementsByTagName('tr'),
-  uri;
-
-for (var i = 0; i < posts.length; i++) {
-  posts[i].onclick = function () {
-    uri = this.getAttribute('data-href');
-    console.log(uri);
-    window.location = `/${uri}`;
-  };
-}
+let table = document.getElementById('blog-posts');
+console.log('table: ', table);
+table.onclick = (e) => {
+  let href = e.target.dataset.href;
+  window.location = `/${href}`;
+};
