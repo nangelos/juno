ee1a0c8238447858120a3085981383fa18c20794
nangelos <nicksangelos@gmail.com>
2020-09-03 16:29:01 -0400

    finishing touches on shell

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let table = document.getElementById('blog-posts');
-console.log('table: ', table);
 table.onclick = (e) => {
   let href = e.target.dataset.href;
   window.location = `${href}`;
+};
+
+let btn = document.getElementById('social-box');
+btn.onclick = (e) => {
+  console.log(e.target);
+  let href = e.target.dataset.href;
+  if (href !== 'mailto:angelos.nick@yahoo.com') {
+    window.open(href, '_blank');
+  } else {
+    window.location = `${href}`;
+  }
 };
