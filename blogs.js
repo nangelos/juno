e18330cbfe943286714d36a81ca074efcb55a1e1
nangelos <nicksangelos@gmail.com>
2020-09-06 14:59:33 -0400

    [constants.js] moving blogPreview array to constants.js

--- blogs.js
+++ blogs.js
@@ -1,18 +1,20 @@
-const blogPreview = [
-  {
-    date: 'Sept 4, 2020',
-    title: 'Why I Built This and Why it Matters to Have a Personal Website',
-    text:
-      'Hello world. My name is blah and I am a blah blah person who likes to blah. I went to the moon in 1969, what can you say about that? I love the...',
-    href: 'blog-posts/sample-post.html',
-  },
-  {
-    date: 'Aug 31, 2020',
-    title: 'How to Make the World Interesting',
-    text: "I'm exceptionally excited about the moon in 1969, and then...",
-    href: 'blog-posts/sample-post.html',
-  },
-];
+// const blogPreview = [
+//   {
+//     date: 'Sept 4, 2020',
+//     title: 'Why I Built This and Why it Matters to Have a Personal Website',
+//     text:
+//       'Hello world. My name is blah and I am a blah blah person who likes to blah. I went to the moon in 1969, what can you say about that? I love the...',
+//     href: 'blog-posts/sample-post.html',
+//   },
+//   {
+//     date: 'Aug 31, 2020',
+//     title: 'How to Make the World Interesting',
+//     text: "I'm exceptionally excited about the moon in 1969, and then...",
+//     href: 'blog-posts/sample-post.html',
+//   },
+// ];
+
+import blogPreview from './constants.js';
 
 const createFooter = (obj) => {
   let blogFooter = document.createElement('div');
---
+++ constants.js
@@ -0,0 +1,17 @@
+const blogPreview = [
+  {
+    date: 'Sept 4, 2020',
+    title: 'Why I Built This and Why it Matters to Have a Personal Website',
+    text:
+      'Hello world. My name is blah and I am a blah blah person who likes to blah. I went to the moon in 1969, what can you say about that? I love the...',
+    href: 'blog-posts/sample-post.html',
+  },
+  {
+    date: 'Aug 31, 2020',
+    title: 'How to Make the World Interesting',
+    text: "I'm exceptionally excited about the moon in 1969, and then...",
+    href: 'blog-posts/sample-post.html',
+  },
+];
+
+export default blogPreview;
