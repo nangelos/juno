f6978098962533aa4cff931b2c1bd83ff063a564
nangelos <nicksangelos@gmail.com>
2020-09-06 17:00:15 -0400

    [books] bookList created in const.js, books.js created to add bookList to table

---
+++ books.js
@@ -0,0 +1,27 @@
+import { bookList } from './constants.js';
+
+const table = document.getElementById('table-body');
+
+const createTableRow = (obj) => {
+  let tableRow = document.createElement('tr');
+  let td1 = document.createElement('td');
+  td1.textContent = obj.title;
+  let td2 = document.createElement('td');
+  td2.textContent = obj.author;
+  tableRow.append(td1);
+  tableRow.append(td2);
+  return tableRow;
+};
+
+const addBooksToTable = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let list = arr[i];
+    for (let j = 0; j < list.books.length; j++) {
+      let book = list.books[j];
+      let tableRow = createTableRow(book);
+      table.append(tableRow);
+    }
+  }
+};
+
+addBooksToTable(bookList);
--- constants.js
+++ constants.js
@@ -14,4 +14,63 @@ const blogPreview = [
   },
 ];
 
+export const recentPosts = [
+  {
+    date: 'Sept 4, 2020',
+    title: 'Why I Built This and Why it Matters to Have a Personal Website',
+    text:
+      'Hello world. My name is blah and I am a blah blah person who likes to blah. I went to the moon in 1969, what can you say about that? I love the...',
+    href: 'blog-posts/sample-post.html',
+  },
+  {
+    date: 'Aug 31, 2020',
+    title: 'How to Make the World Interesting',
+    text: "I'm exceptionally excited about the moon in 1969, and then...",
+    href: 'blog-posts/sample-post.html',
+  },
+  {
+    date: 'Aug 3, 2020',
+    title: 'How I Made a Third Recent Entry',
+    text: "I'm exceptionally excited about the moon in 1969, and then...",
+    href: 'blog-posts/sample-post.html',
+  },
+];
+
+export const bookList = [
+  {
+    year: 2020,
+    books: [
+      {
+        title: 'Enlightenment Now',
+        author: 'Steven Pinker',
+      },
+      {
+        title: 'Expecting Better',
+        author: 'Emily Oster',
+      },
+      {
+        title: 'Lies My Teacher Told Me',
+        author: 'James Loewen',
+      },
+    ],
+  },
+  {
+    year: 2019,
+    books: [
+      {
+        title: 'The Return of Depression Economics',
+        author: 'Paul Krugman',
+      },
+      {
+        title: 'How Markets Fail',
+        author: 'John Cassidy',
+      },
+      {
+        title: 'Aftershock',
+        author: 'Robert Reich',
+      },
+    ],
+  },
+];
+
 export default blogPreview;
