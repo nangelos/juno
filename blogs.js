f2835533cfc59a8becf7ee94999ac5a0843bbe29
nangelos <nicksangelos@gmail.com>
2021-03-21 16:38:04 -0400

    code cleanup

--- blogs.js
+++ blogs.js
@@ -1,19 +1,3 @@
-// const blogPreview = [
-//   {
-//     date: 'Sept 4, 2020',
-//     title: 'Why I Built This and Why it Matters to Have a Personal Website',
-//     text:
-//       'Hello world. My name is blah and I am a blah blah person who likes to blah. I went to the moon in 1969, what can you say about that? I love the...',
-//     href: 'blog-posts/sample-post.html',
-//   },
-//   {
-//     date: 'Aug 31, 2020',
-//     title: 'How to Make the World Interesting',
-//     text: "I'm exceptionally excited about the moon in 1969, and then...",
-//     href: 'blog-posts/sample-post.html',
-//   },
-// ];
-
 import blogPreview from './constants.js';
 
 const createFooter = (obj) => {
